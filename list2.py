62ffb1796d116d130eedaca7b69fd14d48a32250
Lori Henderson <lorim95@gmail.com>
2020-06-06 20:03:05 -0400

    forgot to add that David R helped me

--- list2.py
+++ list2.py
@@ -5,7 +5,7 @@ Kenzie assignment: List2
 """
 # Your name, plus anyone who helped you with this assignment.
 # Give credit where credit is due.
-__author__ = "Lori Henderson"
+__author__ = "Lori Henderson and David R"
 
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
